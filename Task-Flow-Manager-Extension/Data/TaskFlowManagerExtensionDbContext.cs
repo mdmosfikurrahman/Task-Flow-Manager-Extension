77e540b35baae90779e9c975c50096e303416d53
Mosfik <mdmosfikurrahman.cse@gmail.com>
2025-07-10 16:22:57 +0600

    refactor(dbcontext): update namespace and format connection string
    - renamed namespace to align with project naming conventions
    - reformatted connection string for better readability

--- Task-Flow-Manager-Extension/Data/TaskFlowManagerExtensionDbContext.cs
+++ Task-Flow-Manager-Extension/Data/TaskFlowManagerExtensionDbContext.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+﻿using Microsoft.EntityFrameworkCore;
 using Task_Flow_Extension.Models;
 
-namespace Task_Flow_Extension.Data;
+namespace Task_Flow_Manager_Extension.Data;
 
 public partial class TaskFlowManagerExtensionDbContext : DbContext
 {
@@ -22,8 +19,10 @@ public partial class TaskFlowManagerExtensionDbContext : DbContext
     public virtual DbSet<Invoices> invoices { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=root;database=TaskFlowManagerExtension", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https: //go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql(
+            "server=localhost;port=3306;user=root;password=root;database=TaskFlowManagerExtension",
+            ServerVersion.Parse("8.0.40-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- Task-Flow-Manager-Extension/DependencyInjection/StartupConfigurationExtensions.cs
+++ Task-Flow-Manager-Extension/DependencyInjection/StartupConfigurationExtensions.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
-using Task_Flow_Extension.Data;
+using Task_Flow_Manager_Extension.Data;
 using Task_Flow_Manager_Extension.Infrastructure.Config;
 
 namespace Task_Flow_Manager_Extension.DependencyInjection;
